7396831b93c57af39d0a45357fbe9004c2310f4c
Sirsantiag0 <sirsantiago@outlook.es>
2025-08-14 12:42:36 -0500

    cambio

---
+++ schemas/movies.js
@@ -0,0 +1,46 @@
+const z = require("zod");
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: "Movie title must be a string",
+    required_error: "Movie title is required",
+  }),
+  year: z.number().int().min(1900).max(2025),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(1).max(10),
+  poster: z.string().url({
+    message: "Poster must be a valid URL",
+  }),
+  genre: z.array(
+    z.enum([
+      "Action",
+      "Adventure",
+      "Comedy",
+      "Drama",
+      "crime",
+      "Fantasy",
+      "Horror",
+      "Thriller",
+      "Sci-Fi",
+    ]),
+    {
+      required_error: "Movie genre is required",
+      invalid_type_error: "Movie genre must be an array of enum Genre",
+    }
+  ),
+});
+
+function validateMovie(input) {
+  // para valdar que realmente sea un movie
+  return movieSchema.safeParse(input);
+}
+
+function validarPartialMovie(input) {
+  return movieSchema.partial().safeParse(input);
+}
+
+module.exports = {
+  validateMovie,
+  validarPartialMovie,
+};
